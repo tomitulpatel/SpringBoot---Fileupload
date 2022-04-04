dacf67e8e07f9f214efa41f5398a22d649071e76
Mitul Patel <mitulpatel@Mituls-MacBook-Pro.local>
2022-04-04 12:02:22 +0530

    Allowing API to access outside

--- src/main/java/com/fileupload/controller/AttachmentController.java
+++ src/main/java/com/fileupload/controller/AttachmentController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+@CrossOrigin("*")
 @RestController
 public class AttachmentController {
 
--- src/main/java/com/fileupload/controller/UserController.java
+++ src/main/java/com/fileupload/controller/UserController.java
@@ -2,10 +2,12 @@ package com.fileupload.controller;
 
 import com.fileupload.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 public class UserController {
 
